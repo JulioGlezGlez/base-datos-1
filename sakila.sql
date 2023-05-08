b3e09c264b31e975b9ca4cee6605f555ebeff6cb
21raz21 <113715827+21raz21@users.noreply.github.com>
2023-05-08 13:44:04 +0000

    Create sakila.sql
    "Se crea tarea"

---
+++ sakila.sql
@@ -0,0 +1,36 @@
+Se pide:
+
+    Realiza la carga de la BBDD de Sakila, tal y como se muestra en los enlaces proporcionados.
+    Realiza cada una de las siguientes consultas:
+        Actores que tienen de primer nombre Scarlett.
+        Actores que tienen de apellido Johansson.
+        Actores que contengan una O en su nombre.
+        Actores que contengan una O en su nombre y en una A en su apellido.
+        Actores que contengan dos O en su nombre y en una A en su apellido.
+        Actores donde su tercera letra sea B.
+        Ciudades que empiezan por a.
+        Ciudades que acaban por s.
+        Ciudades del country 61.
+        Ciudades del country Spain.
+        Ciudades con nombres compuestos.
+        Películas con una duración entre 80 y 100.
+        Peliculas con un rental_rate entre 1 y 3.
+        Películas con un titulo de más de 12 letras.
+        Peliculas con un rating de PG o G.
+        Peliculas que no tengan un rating de NC-17.
+        Peliculas con un rating PG y duracion de más de 120.
+        ¿Cuantos actores hay?
+        ¿Cuántas ciudades tiene el country Spain?
+        ¿Cuántos countries hay que empiezan por a?
+        Media de duración de peliculas con PG.
+        Suma de rental_rate de todas las peliculas.
+        Pelicula con mayor duración.
+        Película con menor duración.
+        Mostrar las ciudades del country Spain (multitabla).
+        Mostrar el nombre de la película y el nombre de los actores.
+        Mostrar el nombre de la película y el de sus categorías.
+        Mostrar el country, la ciudad y dirección de cada miembro del staff.
+        Mostrar el country, la ciudad y dirección de cada customer.
+        Numero de películas de cada rating
+        Cuantas películas ha realizado el actor ED CHASE.
+        Media de duración de las películas cada categoría.
